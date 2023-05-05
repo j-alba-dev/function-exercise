a2b1ac356dddda48582003c9be4f602a97487c5b
datalyst1127 <96359837+datalyst1127@users.noreply.github.com>
2023-05-04 18:41:00 -0700

    finished q's

--- easy.js
+++ easy.js
@@ -2,34 +2,35 @@
 
 // Write a function that takes in an array of numbers and returns the largest number in the array.
 function findLargestNumber(numbers) {
-  // Your code here
+  let largest = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+  return largest;
 }
 
 const numberArr = [5, 1, 9, 3, 7];
 
 console.log(findLargestNumber(numberArr)); // Output: 9
 
-
 // Write a function that takes in a string and returns the number of vowels in the string.
 function countVowels(str) {
-  // Your code here
+  return str.match(/[aeiou]/gi).length;
 }
-
-const str1 = 'Hello world';
+const str1 = "Hello world";
 
 console.log(countVowels(str1)); // Output: 3
 
-
 // Write a function that takes in an object and a key, and returns the value of that key in the object.
 function getValueByKey(obj, key) {
-  // Your code here
+  return obj[key];
 }
-
 const person = {
-    name: 'John',
-    age: 32,
-    job: 'Software Engineer'
-  };
-  
-console.log(getValueByKey(person, 'age')); // Output: 32
-  
+  name: "John",
+  age: 32,
+  job: "Software Engineer",
+};
+
+console.log(getValueByKey(person, "age")); // Output: 32
